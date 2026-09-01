e50a185631b9831dbb46afe5bfe0cab1658014d8
Hrutuja <hrutb17@gmail.com>
2026-09-02 00:39:46 +0530

    Practiced and display data types

---
+++ 02-DataTypes.py
@@ -0,0 +1,12 @@
+#Here we are Storing value into varisbles and also checking data types of variables where we have stored value.
+
+a = 100
+age = 32
+height = 6.1
+name = "Krish"
+is_student = True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student)) 
+#python auto detects the type of data
