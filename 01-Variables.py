6cc4b7c8ff7abb105e760ddfd1d3f6452126fd18
Hrutuja <hrutb17@gmail.com>
2026-09-02 00:35:12 +0530

    Adding one file where I paracticed storing varibales.

---
+++ 01-Variables.py
@@ -0,0 +1,10 @@
+a = 100 #storing value into a 
+age = 32        #storing value into variable called "age"
+height = 6.1
+name = "Krish"
+is_student = True
+print (age)
+print (a)
+print (height)
+print(name)
+print(is_student)
