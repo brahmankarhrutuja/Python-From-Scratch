a5e1c44c6735a55cddc331f87ec011f0aa205424
Hrutuja <hrutb17@gmail.com>
2026-09-02 22:44:23 +0530

    Practiced some more concepts

---
+++ 03-Conversion.py
@@ -0,0 +1,11 @@
+age = input("Enter your age: ") #iput value is always in string so if we have do operation on it we ahve to convert it into particularly.
+print(age, type(age)) #just checking type of age which is string
+new_age = int(age) #converting age into int from string
+print(new_age, type(new_age)) #printing age and type of age
+#print(new_age)
+
+
+roll_no = input("Enter your roll number:")
+rollno = int(roll_no)
+rollno = float(roll_no)
+print(roll_no)
